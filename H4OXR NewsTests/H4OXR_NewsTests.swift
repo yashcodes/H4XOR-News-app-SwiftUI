5db7d3986a887fb86d2905689c473166279022fb
Yashvardhan Sharma <sharmayashvardhan6@gmail.com>
2020-07-20 16:38:54 +0530

    Initial Commit

---
+++ H4OXR NewsTests/H4OXR_NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  H4OXR_NewsTests.swift
+//  H4OXR NewsTests
+//
+//  Created by Yash  on 20/07/20.
+//  Copyright © 2020 Yash . All rights reserved.
+//
+
+import XCTest
+@testable import H4OXR_News
+
+class H4OXR_NewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
